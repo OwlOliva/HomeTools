7a5bd96c94ffe05a4b20512e2c775d2b20ad1365
OwlOliva <93000186+OwlOliva@users.noreply.github.com>
2025-10-18 20:07:09 +0500

    Добавление eslint

--- src/main.js
+++ src/main.js
@@ -1,37 +1,24 @@
-import './style.css'
-import {validateHandler} from './components/Form/form';
+import "./style.css";
+import { validateHandler, CloseModal, OpenModal } from "./components/Form/Form";
 
 const modal = document.querySelector(".modal");
 
-const overlay =  document.querySelector('.overlay');
-const button = document.querySelector('.buttonModal');
-const buttonSubmit = document.querySelector('.buttonSubmit');
+const overlay = document.querySelector(".overlay");
+const button = document.querySelector(".buttonModal");
+const buttonSubmit = document.querySelector(".buttonSubmit");
 
-const CloseModal = (modal,overlay) => {
-  modal.classList.remove('modalOpened');
-  overlay.classList.remove('modalOpened');
-  document.body.style.overflow = '';
-}
 
-const OpenModal = (modal,overlay) => {
-  modal.classList.add('modalOpened');
-  
-  overlay.classList.add('modalOpened');
-  document.body.style.overflow = 'hidden';
-}
-
-button.addEventListener("click",()=> {
-  OpenModal(modal,overlay)
-} );
-
-overlay.addEventListener('click',()=> {
-  CloseModal(modal,overlay)
+button.addEventListener("click", () => {
+  OpenModal(modal, overlay);
 });
-const closeButton = document.querySelector('.closeButton');
-closeButton?.addEventListener('click',()=> {
-  CloseModal(modal,overlay)
+
+overlay.addEventListener("click", () => {
+  CloseModal(modal, overlay);
+});
+const closeButton = document.querySelector(".closeButton");
+closeButton?.addEventListener("click", () => {
+  CloseModal(modal, overlay);
 });
 
 
-buttonSubmit.addEventListener('click',validateHandler);
-export {CloseModal, OpenModal}
+buttonSubmit.addEventListener("click", validateHandler);
